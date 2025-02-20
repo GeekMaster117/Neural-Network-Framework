e40b84a0c211ae1c2a5bbf0d507a6e02ecf44e7d
GeekMaster117 <88583239+GeekMaster117@users.noreply.github.com>
2025-02-20 09:56:14 +0000

    Modified add function to return result matrix instead of applying sum on it self. Added dot method with no functionality for now.

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -13,6 +13,11 @@ class Matrix
     public:
         Matrix(int rows, int cols): rows(rows), cols(cols), matrix(rows, std::vector<double>(cols, 0.00)) {}
 
+        Matrix(int rows, int cols, std::vector<std::vector<double>> vector): rows(rows), cols(cols), matrix(rows, std::vector<double>(cols, 0.00))
+        {
+            this -> inputData(vector);
+        }
+
         std::vector<std::vector<double>> getVector()
         {
             return this -> matrix;
@@ -27,7 +32,7 @@ class Matrix
         {
             if(static_cast<size_t>(this -> rows) != matrix.size() || static_cast<size_t>(this -> cols) != matrix[0].size())
                 throw std::invalid_argument("Input Matrix Dimensions "
-                "(" + std::to_string(rows) + ", " + std::to_string(cols) + ")"
+                "(" + std::to_string(matrix.size()) + ", " + std::to_string(matrix[0].size()) + ")"
                 " do not match with Matrix Dimensions"
                 "(" + std::to_string(this -> rows) + ", " + std::to_string(this -> cols) + ")");
 
@@ -48,14 +53,32 @@ class Matrix
             }
         }
 
-        void add(Matrix* matrix)
+        Matrix* add(Matrix* matrix)
         {
             if(this -> rows != matrix -> rows || this -> cols != matrix -> cols)
-                throw std::invalid_argument("Matrix Dimensions do not match");
+                throw std::invalid_argument("Input Matrix Dimensions "
+                "(" + std::to_string(matrix -> rows) + ", " + std::to_string(matrix -> cols) + ")"
+                " do not match with Matrix Dimensions"
+                "(" + std::to_string(this -> rows) + ", " + std::to_string(this -> cols) + ")");
+
+            std::vector<std::vector<double>> resultVector = std::vector<std::vector<double>>(this -> rows, std::vector<double>(this -> cols, 0.00));
 
             for(int i = 0; i < this -> rows; ++i)
                 for(int j = 0; j < this -> cols; ++j)
-                    this -> matrix[i][j] += matrix -> getValue(i, j);
+                    resultVector[i][j] += matrix -> getValue(i, j);
+
+            return new Matrix(this -> rows, this -> cols, resultVector);
+        }
+
+        void dot(Matrix* matrix)
+        {
+            if(this -> cols != matrix -> rows)
+                throw std::invalid_argument("Input Matrix Rows "
+                "(" + std::to_string(rows) + ", " + "~)"
+                " do not match with Matrix Columns"
+                "(~" + ", " + std::to_string(this -> cols) + ")");
+
+            
         }
 };
 
@@ -63,13 +86,6 @@ int main()
 {
     std::vector<std::vector<double>> vector1 = std::vector<std::vector<double>>(2, std::vector<double>(2, 0.00));
     std::vector<std::vector<double>> vector2 = std::vector<std::vector<double>>(2, std::vector<double>(2, 0.00));
-
-    for(int i = 0; i < 2; ++i)
-        for(int j = 0; j < 2; ++j)
-        {
-            vector1[i][j] = 1.00;
-            vector2[i][j] = 2.00;
-        }
 
     Matrix* matrix1 = new Matrix(2, 2);
     Matrix* matrix2 = new Matrix(2, 2);
@@ -84,8 +100,8 @@ int main()
 
     std::cout << std::endl;
 
-    matrix1 -> add(matrix2);
-    matrix1 -> displayData();
+    Matrix* sumMatrix = matrix1 -> add(matrix2);
+    sumMatrix -> displayData();
     
     return 0;
 }

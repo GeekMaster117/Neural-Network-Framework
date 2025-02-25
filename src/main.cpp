d5d8b9ac77459e79ffb95a217a9cc99b1894f78f
GeekMaster117 <88583239+GeekMaster117@users.noreply.github.com>
2025-02-25 09:05:00 +0000

    Added multiple layers to example

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,18 @@
 
 int main()
 {
-    Matrix inputs = Matrix(10, 3);
+    Matrix inputs = Matrix(3, 3);
     inputs.fillMatrix(1.00);
 
-    Layer inputLayer = Layer(3, 4);
+    Layer layer1 = Layer(3, 4);
+    Layer layer2 = Layer(4, 4);
+    Layer layer3 = Layer(4, 2);
 
-    Matrix outputs = inputLayer.forward(&inputs);
-    outputs.displayData();
+    Matrix layer1Outputs = layer1.forward(&inputs);
+    Matrix layer2Outputs = layer2.forward(&layer1Outputs);
+    Matrix layer3Outputs = layer3.forward(&layer2Outputs);
+
+    layer3Outputs.displayData();
     
     return 0;
 }

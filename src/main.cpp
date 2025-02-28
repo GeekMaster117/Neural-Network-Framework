88e81ba6e75802dec5f2ea77dd1f226c37b93968
GeekMaster117 <88583239+GeekMaster117@users.noreply.github.com>
2025-02-28 07:42:10 +0000

    Updated example code to display epoch and accuracy.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
+#include <string>
+
 #include "layer.hpp"
 #include "layer_utils.h"
 #include "csv_utils.h"
 #include "activation.h"
+#include "loss.h"
 
 int main()
 {
@@ -12,7 +16,7 @@ int main()
 
     for(unsigned int i = 0; i < getBatchCount(true); ++i)
     {
-        Matrix inputs = getDatasetBatch(i, true);
+        Matrix inputs = getSamples(i, true);
 
         Matrix layer1Outputs = layer1.forward(&inputs);
         Matrix activation1Outputs = activationReLU(&layer1Outputs);
@@ -23,7 +27,10 @@ int main()
         Matrix layer3Outputs = layer3.forward(&layer2Outputs);
         Matrix activation3Outputs = activationSoftmax(&layer3Outputs);
     
-        activation3Outputs.displayData();
+        Matrix labels = getLabels(i, true);
+
+        std::cout << "Epoch: " << i + 1 << std::endl;
+        std::cout << "Accuracy: " << calculateAccuracy(&activation3Outputs, &labels) << std::endl << std::endl;
     }
     
     return 0;

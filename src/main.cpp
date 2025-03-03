81d3dd3406dffd908d9a49dacf4b731909b10be1
GeekMaster117 <88583239+GeekMaster117@users.noreply.github.com>
2025-03-03 08:47:17 +0000

    Updated example code to add complete backward propogation

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,10 @@ int main()
         Matrix layer1Outputs = layer1.forward(&inputs);
         Matrix activation1Outputs = activationReLU(&layer1Outputs);
 
-        Matrix layer2Outputs = layer2.forward(&layer1Outputs);
+        Matrix layer2Outputs = layer2.forward(&activation1Outputs);
         Matrix activation2Outputs = activationReLU(&layer2Outputs);
 
-        Matrix layer3Outputs = layer3.forward(&layer2Outputs);
+        Matrix layer3Outputs = layer3.forward(&activation2Outputs);
         Matrix activation3Outputs = activationSoftmax(&layer3Outputs);
     
         Matrix labels = getLabels(i, true);
@@ -33,7 +33,14 @@ int main()
 
         std::cout << "Training..." << std::endl;
 
-        activationSoftmaxCategoricalLossBackward(&activation3Outputs, &labels);
+        Matrix dLoss_dLayer3Outputs = activationSoftmaxCategoricalLossBackward(&activation3Outputs, &labels);
+        Matrix dLoss_dactivation2Outputs = layer3.backward(&activation2Outputs, &dLoss_dLayer3Outputs);
+        
+        Matrix dLoss_dLayer2Outputs = activationReLUBackward(&dLoss_dactivation2Outputs);
+        Matrix dLoss_dactivation1Outputs = layer2.backward(&activation1Outputs, &dLoss_dLayer2Outputs);
+
+        Matrix dLoss_dLayer1Outputs = activationReLUBackward(&dLoss_dactivation1Outputs);
+        layer1.backward(&inputs, &dLoss_dLayer1Outputs);
 
         std::cout << "Training Complete" << std::endl << std::endl;
     }

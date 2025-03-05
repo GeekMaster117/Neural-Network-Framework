462b4acbef043e51c6c674a5c2ff7396953df4a5
GeekMaster117 <88583239+GeekMaster117@users.noreply.github.com>
2025-03-05 16:15:58 +0000

    Updated training code to load dataset using chunks

--- src/train.cpp
+++ src/train.cpp
@@ -13,38 +13,46 @@ int main()
     Layer layer2 = Layer(256, 256);
     Layer layer3 = Layer(256, 10);
 
-    for(unsigned int epoch = 0; epoch < getBatchCount(trainDatasetPath); ++epoch)
+    unsigned int epoch = 0;
+    CSV dataset = CSV(trainDatasetPath);
+
+    for(unsigned int chunk = 0; chunk < dataset.getChunkCount(); ++chunk)
     {
-        Matrix inputs = getSamples(epoch, trainDatasetPath);
-        Matrix inputsNormalized = inputs.div(255.00);
+        dataset.loadChunk(chunk);
 
-        Matrix layer1Outputs = layer1.forward(&inputsNormalized);
-        Matrix activation1Outputs = activationReLU(&layer1Outputs);
+        for(unsigned int batch = 0; batch < dataset.getBatchCount(); ++batch, ++epoch)
+        {
+            Matrix inputs = dataset.getSamples(batch);
+            Matrix inputsNormalized = inputs.div(255.00);
 
-        Matrix layer2Outputs = layer2.forward(&activation1Outputs);
-        Matrix activation2Outputs = activationReLU(&layer2Outputs);
+            Matrix layer1Outputs = layer1.forward(&inputsNormalized);
+            Matrix activation1Outputs = activationReLU(&layer1Outputs);
 
-        Matrix layer3Outputs = layer3.forward(&activation2Outputs);
-        Matrix activation3Outputs = activationSoftmax(&layer3Outputs);
+            Matrix layer2Outputs = layer2.forward(&activation1Outputs);
+            Matrix activation2Outputs = activationReLU(&layer2Outputs);
+
+            Matrix layer3Outputs = layer3.forward(&activation2Outputs);
+            Matrix activation3Outputs = activationSoftmax(&layer3Outputs);
     
-        Matrix labels = getLabels(epoch, trainDatasetPath);
+            Matrix labels = dataset.getLabels(batch);
 
-        std::cout << "Epoch: " << epoch + 1 << std::endl;
-        std::cout << "Loss: " << calculateCategoricalLoss(&activation3Outputs, &labels) << std::endl;
-        std::cout << "Accuracy: " << calculateAccuracy(&activation3Outputs, &labels) << std::endl;
+            std::cout << "Epoch: " << epoch + 1 << std::endl;
+            std::cout << "Loss: " << calculateCategoricalLoss(&activation3Outputs, &labels) << std::endl;
+            std::cout << "Accuracy: " << calculateAccuracy(&activation3Outputs, &labels) << std::endl;
 
-        std::cout << "Training..." << std::endl;
+            std::cout << "Training..." << std::endl;
 
-        Matrix dLoss_dLayer3Outputs = activationSoftmaxCategoricalLossBackward(&activation3Outputs, &labels);
-        Matrix dLoss_dactivation2Outputs = layer3.backward(&activation2Outputs, &dLoss_dLayer3Outputs, epoch);
+            Matrix dLoss_dLayer3Outputs = activationSoftmaxCategoricalLossBackward(&activation3Outputs, &labels);
+            Matrix dLoss_dactivation2Outputs = layer3.backward(&activation2Outputs, &dLoss_dLayer3Outputs, epoch);
         
-        Matrix dLoss_dLayer2Outputs = activationReLUBackward(&layer2Outputs, &dLoss_dactivation2Outputs);
-        Matrix dLoss_dactivation1Outputs = layer2.backward(&activation1Outputs, &dLoss_dLayer2Outputs, epoch);
+            Matrix dLoss_dLayer2Outputs = activationReLUBackward(&layer2Outputs, &dLoss_dactivation2Outputs);
+            Matrix dLoss_dactivation1Outputs = layer2.backward(&activation1Outputs, &dLoss_dLayer2Outputs, epoch);
 
-        Matrix dLoss_dLayer1Outputs = activationReLUBackward(&layer1Outputs, &dLoss_dactivation1Outputs);
-        layer1.backward(&inputsNormalized, &dLoss_dLayer1Outputs, epoch);
+            Matrix dLoss_dLayer1Outputs = activationReLUBackward(&layer1Outputs, &dLoss_dactivation1Outputs);
+            layer1.backward(&inputsNormalized, &dLoss_dLayer1Outputs, epoch);
 
-        std::cout << "Training Complete" << std::endl << std::endl;
+            std::cout << "Training Complete" << std::endl << std::endl;
+        }
     }
 
     layer1.saveParams("layer1");
